daf16605d9cba1cf61dcab4e68ed1e2329ea8cb0
lukas.anderlind1 <lukas.anderlind1@skola.taby.se>
2024-09-09 14:51:33 +0200

    lektion 09/09/24

--- siffor.py
+++ siffor.py
@@ -1,1 +1,11 @@
-CLASS   
+class Elev:
+    def __init__(self, namn, alder, godkand):
+        self.namn = namn 
+        self.alder = alder 
+        self.glad = godkand 
+
+elev1 = Elev("Anna", 16, True)
+elev2 = Elev("Erik", 17, False)
+
+print(f"{elev1.namn} är {elev1.alder} år och glad: {elev1.glad}")
+print(f"{elev2.namn} är {elev2.alder} år och glad: {elev2.glad}")
--- under1000delbart med 7.py
+++
@@ -1,7 +0,0 @@
-
-i = 0
-for i in range (1, 1000):
-    if i % 7 == 0:
-        print (i)   #Testar en liten ändring
-
-
